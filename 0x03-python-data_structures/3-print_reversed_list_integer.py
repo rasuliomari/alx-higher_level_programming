86a86988916f88b915c8b0083599db7708405158
rasuliomari <rasuli707046@gmail.com>
2023-08-14 04:04:18 -0700

    task3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    if type(my_list) is list:
+        my_list.reverse()
+        for list1 in my_list:
+            print("{:d}".format(list1))
