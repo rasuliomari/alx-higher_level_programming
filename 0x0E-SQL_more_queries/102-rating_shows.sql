8fe87beb7228f93c5dfb32fd8677651be93c44a9
rasuliomari <rasuli707046@gmail.com>
2023-10-18 15:47:56 -0700

    task19

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,18 +1,7 @@
--- SQL: script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
-SELECT DISTINCT `title`
+-- SQL: Script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+SELECT `title`, SUM(`rate`) AS `rating`
   FROM `tv_shows` AS t
-       LEFT JOIN `tv_show_genres` AS s
-       ON s.`show_id` = t.`id`
-
-       LEFT JOIN `tv_genres` AS g
-       ON g.`id` = s.`genre_id`
-       WHERE t.`title` NOT IN
-             (SELECT `title`
-                FROM `tv_shows` AS t
-	             INNER JOIN `tv_show_genres` AS s
-		     ON s.`show_id` = t.`id`
-
-		     INNER JOIN `tv_genres` AS g
-		     ON g.`id` = s.`genre_id`
-		     WHERE g.`name` = "Comedy")
- ORDER BY `title`;
+       INNER JOIN `tv_show_ratings` AS r
+       ON t.`id` = r.`show_id`
+ GROUP BY `title`
+ ORDER BY `rating` DESC;
