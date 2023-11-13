d44a5277d5f6d883bd9c4bf43b79c88018e2dc07
rasuliomari <rasuli707046@gmail.com>
2023-11-13 13:57:50 -0800

    task8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// The script that prints a square.
+
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('X'.repeat(parseInt(process.argv[2])));
+  }
+}
