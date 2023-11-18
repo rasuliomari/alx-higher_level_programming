b815ea0983e4855cff2b5cea10fea95bf793fd22
rasuliomari <rasuli707046@gmail.com>
2023-11-18 09:39:21 -0800

    task0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
+
 """
 This script that lists all states from the database hbtn_0e_0_usa:
 """
+import sys
+import MySQLdb
+
 if __name__ == "__main__":
     """
     MySQL Connection
