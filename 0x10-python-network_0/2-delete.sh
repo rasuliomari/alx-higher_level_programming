9ed2cba186e9e40746bcace935224b91d6ecb60a
rasuliomari <rasuli707046@gmail.com>
2023-11-30 13:33:34 -0800

    task2

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Use curl to send a DELETE request to the specified URL and display the body
-curl -s -X DELETE "$1" -w "\n"
+# # Use curl to send a DELETE request to the specified URL and display the body
+curl -sL "$1" -X DELETE
