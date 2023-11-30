3a1dda6151b25a6dc4cc92133fa411ededad8777
rasuliomari <rasuli707046@gmail.com>
2023-11-30 13:52:40 -0800

    task3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-
-# Use curl to send an OPTIONS request to the specified URL and display allowed methods
-curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
+# Script that takes an URL and shows the Allowed OPTIONS
+curl -sI "$1" | grep Allow | cut -d " " -f 2-
