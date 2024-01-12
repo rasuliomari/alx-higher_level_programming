ff41efe9ec2e8843f0e8554f619e3e8a43b9db7b
rasuliomari <rasuli707046@gmail.com>
2024-01-12 12:22:56 -0800

    5. List of elements

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$("#add_item").click(function() {
+                const newItem = $("<li>Item</li>");
+
+                $(".my_list").append(newItem);
+        });
+});
